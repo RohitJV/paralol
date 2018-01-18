630176221073f44d7113c0dd23d42dd138391846
rohitjv <jvrohit1201@gmail.com>
2018-01-17 21:41:29 -0600

    Guassian completed

--- matrix/gaussian.c
+++ matrix/gaussian.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
 float a[10][10], b[10], y[10];
+int n;
 
+
+// implementation according to the textbook
+/*
+    Ax=y -> Ux=b, where U is a upper triangular matrix
+    In the below code, we replace elements in A with elements of U
+*/
 void gaussian_elimination() {
     int i,j,k;
     for(i=0;i<n;i++) {
@@ -12,6 +19,18 @@ void gaussian_elimination() {
         }
         y[i]=b[i]/a[i][i];
         a[i][i]=1;
+
+        /*
+        The below invariant establishes that the matrix is indeed a Upper Triangular matrix:
+        For each row below, apply operations such that the element a[.][i]=0
+        */
+        for(j=i+1;j<n;j++) {
+            for(k=i+1;k<n;k++) {
+                a[j][k] = a[j][k] - a[i][k]*a[j][i];
+            }
+            a[j][i]=0;
+            b[j]=b[j]-a[j][i]*y[i];
+        }
     }
 }
 
@@ -32,5 +51,14 @@ int main() {
 
     gaussian_elimination();
 
-
+    printf("Elements of Upper Triangular matrix 'U' in 'Ux=y' : \n");
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++)
+            printf("%f ",a[i][j]);
+        printf("\n");
+    }
+    printf("Elements of 'y' in 'Ux=y' : \n");
+    for(i=0;i<n;i++) {
+        printf("%f ",y[i]);
+    }
 }
